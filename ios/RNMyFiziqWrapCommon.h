df4a8bf61f46ff7491a3f069ad88097e7f19065e
phillipc-myfiziq <phillip@myfiziq.com>
2020-04-24 17:22:54 +0800

    Fixed example

--- ios/RNMyFiziqWrapCommon.h
+++
@@ -1,7 +0,0 @@
-
-#import <MyFiziqSDKCommon/MyFiziqCommon.h>
-
-@interface RNMyFiziqWrapCommon : MyFiziqCommon <MyFiziqCommonProtocol>
-@property (strong, nonatomic) NSString *sdkCssPathLocal;
-+ (instancetype)shared;
-@end
